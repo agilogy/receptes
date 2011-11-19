a381a8e62c54d1ca0d7081635e38e839a88f26f2
Jose Raya <mail@joseraya.net>
2011-11-19 23:26:21 +0100

    Moved DB configuration to env. var

--- src/receptes/models/recipes.clj
+++ src/receptes/models/recipes.clj
@@ -4,11 +4,19 @@
 ;ToDo: Estudiar la posibilidad de usar defrecord
 ;(defrecord Recipe [name ingredients instructions owner])
 
-(def conn (make-connection "receptes"
-                            :host "dbh76.mongolab.com"
-                            :port 27767))
+;;(let [[all user password host port database ] 
+;;		(re-matches #"mongodb://(.*):(.*)@(.*):(.*)/(.*)" (System/getenv "MONGOLAB_URI") )]
+;;	(def conn (make-connection database :host host :port port ))
+;;	(authenticate conn user password))
 
-(authenticate conn ZZZZ ZZZZ)
+(let [uri (java.net.URI. (System/getenv "MONGOLAB_URI"))
+    host (.getHost uri)
+    port (if (pos? (.getPort uri)) (.getPort uri) 27000)
+    database (.substring (.getPath uri) 1)
+    [userinfo username password] (re-matches #"(.*):(.*)" (.getUserInfo uri))]
+		(def conn (make-connection database :host host :port port ))
+		(authenticate conn username password))
+
 
 (defn add-recipe [name ingredients instructions owner]
 	(with-mongo conn
